9f87630e328ad84ff0ac4cd030c4f306fe59538f
Hyago Rodrigues <hyagof16@gmail.com>
2024-10-13 13:22:12 -0300

    crianda funcoes e adicionando tipos

--- screen-match.php
+++ screen-match.php
@@ -1,5 +1,19 @@
 <?php
 
+function exibe_mensagem_ano_lancamento (int $ano): void {
+    if ($ano > 2022) {
+        echo "Esse filme é um lançamento\n";
+    } elseif($ano > 2020 && $ano <= 2022) {
+        echo "Esse filme ainda é novo\n";
+    } else {
+        echo "Esse filme não é um lançamento\n";
+    }
+}
+
+function incluido_no_plano(bool $plano, int $ano): bool {
+    return $plano || $ano < 2020;
+}
+
 echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
@@ -16,19 +30,13 @@ for ($contador = 1; $contador < $argc; $contador++) {
 $notaFilme = array_sum($notas) / $quantidadeDeNotas;
 $planoPrime = true;
 
-$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+$incluidoNoPlano = incluido_no_plano($planoPrime, $anoLancamento);
 
 echo "Nome do filme: " . $nomeFilme . "\n";
 echo "Nota do filme: $notaFilme\n";
 echo "Ano de lançamento: $anoLancamento\n";
 
-if ($ano > 2022) {
-    echo "Esse filme é um lançamento\n";
-} elseif($ano > 2020 && $ano <= 2022) {
-    echo "Esse filme ainda é novo\n";
-} else {
-    echo "Esse filme não é um lançamento\n";
-}
+exibe_mensagem_ano_lancamento($anoLancamento);
 
 $genero = match ($nomeFilme) {
     "Top Gun - Maverick" => "ação",
